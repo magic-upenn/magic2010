38e6834007d7728233f08e28c81cce951b80f124
akushley <akushley@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-04-25 21:41:56 +0000

    adding receive test scripts
    git-svn-id: svn://localhost:5050/magic2010/trunk@36 61389559-6c2a-4883-acdc-2d27d9f19ada

---
+++ matlab/Serialization/receiveEncoders.m
@@ -0,0 +1,18 @@
+addpath( [ getenv('VIS_DIR') '/ipc' ] )
+
+robotIdStr = getenv('ROBOT_ID');
+
+ipcAPIConnect;
+ipcAPISubscribe(['Robot' robotIdStr '/Encoders']);
+
+
+while(1)
+  msgs = ipcAPIReceive(10);
+  len = length(msgs);
+  if len > 0
+    for i=1:len
+      encoders = MagicEncodersPacketSerializer('deserialize',msgs(i).data)
+      %fprintf(1,'.');
+    end
+  end
+end
