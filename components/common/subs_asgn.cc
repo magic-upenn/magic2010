373ff370ea167c67dc8835a8ad4093903fb49d62
ddlee <ddlee@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-06-06 17:07:44 +0000

    Fast Mex routines to manipulate arrays
    git-svn-id: svn://localhost:5050/magic2010/trunk@216 61389559-6c2a-4883-acdc-2d27d9f19ada

---
+++ components/common/subs_asgn.cc
@@ -0,0 +1,51 @@
+/*
+  y = subs_asgn(x,i,j,v);
+
+  MEX file to compute x(sub2ind(i,j)) = v;
+
+  Note: This routine accumulates repeated indices and changes
+  the matrix x directly in memory.
+
+  Daniel D. Lee, 06/2007
+  <ddlee@seas.upenn.edu>
+*/
+
+#include "mex.h"
+#include <math.h>
+
+void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
+{
+  /* Check arguments */
+  if (nrhs < 4)
+    mexErrMsgTxt("Need four input arguments.");
+
+  double *x = mxGetPr(prhs[0]);
+  int mx = mxGetM(prhs[0]);
+  int nx = mxGetN(prhs[0]);
+
+  double *indexI = mxGetPr(prhs[1]);
+  int mi = mxGetM(prhs[1]);
+  int ni = mxGetN(prhs[1]);
+
+  double *indexJ = mxGetPr(prhs[2]);
+  if ((mxGetM(prhs[2]) != mi) || (mxGetN(prhs[2]) != ni)) {
+    mexErrMsgTxt("Index arrays need to be the same size");
+  }
+
+  double *value = mxGetPr(prhs[3]);
+  int nvalue = mxGetNumberOfElements(prhs[3]);
+
+  for (int k = 0; k < mi*ni; k++) {
+    int i = (int) round(indexI[k]) - 1; // zero-indexing;
+    int j = (int) round(indexJ[k]) - 1; // zero-indexing;
+
+    if ((i >= 0) && (i < mx) && (j >= 0) && (j < nx)) {
+      int index = mx*j + i;
+      double v = (nvalue == 1) ? value[0] : value[k];
+      x[index] = v;
+    }
+  }
+  
+  //  plhs[0] = (mxArray *)prhs[0];
+  plhs[0] = mxCreateDoubleMatrix(0,0,mxREAL);
+}
