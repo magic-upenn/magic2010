ce4286487bab66543cd0724f678043025a4ea47d
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-06-06 02:38:10 +0000

    Testing vertical lidar processing
    git-svn-id: svn://localhost:5050/magic2010/trunk@208 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/slam/test_lidar1.m
+++ components/slam/test_lidar1.m
@@ -4,10 +4,11 @@ SetMagicPaths;
 ipcInit('localhost');
 
 lidar1Init;
-%servo1Init;
+servo1Init;
 
 ipcReceiveSetFcn(GetMsgName('Lidar1'),      @slamProcessLidar1);
-%ipcReceiveSetFcn(GetMsgName('Servo1'),      @slamProcessServo1);
+ipcReceiveSetFcn(GetMsgName('ImuFiltered'), @ipcRecvImuFcn);
+ipcReceiveSetFcn(GetMsgName('Servo1'),      @slamProcessServo1);
 
 loop = 1;
 while loop,
