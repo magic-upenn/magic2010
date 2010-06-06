44b2405f0090b8b1987ef828cb0deb77e45d71b1
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-06-06 02:16:23 +0000

    Modified servo start/stop commands
    git-svn-id: svn://localhost:5050/magic2010/trunk@205 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/examples/sendServoStop.m
+++ components/examples/sendServoStop.m
@@ -7,8 +7,8 @@ ipcAPIDefine(servoMsgName,MagicServoControllerCmdSerializer('getFormat'));
 
 
 servoCmd.id           = 1;
-servoCmd.mode         = 1; %0 for point mode (minAngle is the goal), 1 for servo mode
-servoCmd.minAngle     = -35;
+servoCmd.mode         = 0; %0 for point mode (minAngle is the goal), 1 for servo mode
+servoCmd.minAngle     = 0;
 servoCmd.maxAngle     = 35;
 servoCmd.speed        = 100;
 servoCmd.acceleration = 300;
