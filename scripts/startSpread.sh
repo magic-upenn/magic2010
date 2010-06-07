9c8346920414e01cf9289fedb76bd77c8dfdb1fa
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-06-07 20:49:36 +0000

    spread daemon
    git-svn-id: svn://localhost:5050/magic2010/trunk@247 61389559-6c2a-4883-acdc-2d27d9f19ada

--- scripts/startSpread.sh
+++ scripts/startSpread.sh
@@ -34,11 +34,7 @@ StartFunction ()
 
 # List session names and commands for screen, delimited by ":"
 StartFunction <<EOF
-central:nice -n -5 central -s
 spread:/usr/local/sbin/spread -l y
-hokuyo0:$MAGIC_DIR/drivers/Hokuyo/runHokuyo /dev/ttyACM0
-hokuyo1:$MAGIC_DIR/drivers/Hokuyo/runHokuyo /dev/ttyACM1
-microGateway:nice -n -5 $MAGIC_DIR/drivers/MicroGateway/runMicroGateway
 EOF
 
 exit
