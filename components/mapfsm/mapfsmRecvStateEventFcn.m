82b45323331ba3f9a3a8ba5e66951a6e8dd1e7cb
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-06-09 00:51:37 +0000

    from robot
    git-svn-id: svn://localhost:5050/magic2010/trunk@281 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/mapfsm/mapfsmRecvStateEventFcn.m
+++ components/mapfsm/mapfsmRecvStateEventFcn.m
@@ -1,7 +1,11 @@
-function ipcRecvDriveFcn(data, name)
+function mapfsmRecvStateEventFcn(data, name)
 
 global MP
 
 if ~isempty(data)
+  event = deserialize(data);
+if isstr(event),
+  disp(sprintf('StateEvent: %s',event));
   MP.sm = setEvent(MP.sm, deserialize(data));
+end
 end
