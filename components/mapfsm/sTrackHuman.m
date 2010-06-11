ef9c6e29feeac9ef60eca33a1e5db3b123812c8c
ddlee <ddlee@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-06-11 04:28:57 +0000

    git-svn-id: svn://localhost:5050/magic2010/trunk@369 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/mapfsm/sTrackHuman.m
+++ components/mapfsm/sTrackHuman.m
@@ -36,7 +36,6 @@ switch event
    if ~isempty(TRACKS) && ~isempty(TRACKS.xs),
      dx = TRACKS.xs - DATA.x;
      dy = TRACKS.ys - DATA.y;
-     [dx dy]
      dist = sqrt(dx.^2 + dy.^2);
      [dmin, imin] = min(dist);
      if (dmin < DATA.distThreshold),
