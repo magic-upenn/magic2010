274d3461536760f8adc3ece2f1be60d93e746f5c
jbutzke <jbutzke@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-09-20 05:37:16 +0000

    [JMB] delete unused files
    git-svn-id: svn://localhost:5050/magic2010/trunk@441 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/exploration/generate_map.h
+++
@@ -1,6 +0,0 @@
-#ifndef GENERATE_MAP_JMB
-#define GENERATE_MAP_JMB
-void mapfill(unsigned char cost_map[], int size_x, int size_y, int startx, int starty);
-
-void generate_map(int sizex, int sizey, int obs_num, int obs_size, int point_num);
-#endif
