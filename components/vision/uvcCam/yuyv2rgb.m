a48663b8c37fb5348c812d1803ab6d2a487a835b
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-09-25 19:08:28 +0000

    yuyv2rgb now upsamples instead of downsamples
    git-svn-id: svn://localhost:5050/magic2010/trunk@464 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/vision/uvcCam/yuyv2rgb.m
+++ components/vision/uvcCam/yuyv2rgb.m
@@ -1,7 +1,15 @@
-function rgb=yuyv2rgb(yuyv);
+function [rgb] =yuyv2rgb(yuyv);
 
 siz = size(yuyv);
+newsiz = siz * diag([2,1]); 
 yuyv_u8 = reshape(typecast(yuyv(:), 'uint8'), [4 siz]);
+y1 = permute(yuyv_u8(1,:,:),[2,3,1]);; 
+y2 = permute(yuyv_u8(3,:,:),[2,3,1]);
+cb = imresize(permute(yuyv_u8(2,:,:),[2,3,1]),newsiz);
+cr = imresize(permute(yuyv_u8(4,:,:),[2,3,1]),newsiz);
+y = zeros(newsiz); 
+y(1:2:end,:) = y1;
+y(2:2:end,:) = y2;
 ycbcr = yuyv_u8([1 2 4], :, 1:2:end);
 ycbcr = permute(ycbcr, [3 2 1]);
 rgb = ycbcr2rgb(ycbcr);
