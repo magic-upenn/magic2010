f27a4eb939ee1edcb98d8c6a594bc08b5e6969c2
jbutzke <jbutzke@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-09-26 22:36:53 +0000

    [JMB] controller update to take continuous paths
    git-svn-id: svn://localhost:5050/magic2010/trunk@469 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/mapfsm/ctrlsim/ctrlsim.m
+++ components/mapfsm/ctrlsim/ctrlsim.m
@@ -16,15 +16,26 @@ vel = 1;
 xlist = [];
 ylist = [];
 
-xtarl = [0 10 -10 -10  10  5 -5 10 -10];
-ytarl = [0  9   9  -9  -9  4 -4  0   0];
+
+xtarl = [0:.1:9.9 10:-.1:-9.9 -10*ones(1,201)  ];
+ytarl = [0:.1:9.9 10*ones(1,200) 10:-.1:-10 ];
+
+
+
+% xtarl = [0 1 2 3 4 5 6 7 8 9 10 9 8 7 6 5 4 3 2 1 0 -1 -2 -3 -4 -5 -6 -7 -8 -9 -10 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -8 -7 -6 -5 -4 -3 -2 -1  0  1  2  3  4  5  6  7  8  9 10  9  9  9  8  8  8  7  7  7  6  6  6  5  4  3  2  1  0 -1 -2 -3 -4 -5 -4 -3 -2 -1  0  1  2  3  4  5  6  7  8  9 10 ];
+% ytarl = [0 1 2 3 4 5 6 7 8 9 9  9 9 9 9 9 9 9 9 9 9  9  9  9  9  9  9  9  9  9   9  8  7  6  5  4  3  2  1  0 -1 -2 -3 -4 -5 -6 -7 -8 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -9 -8 -7 -6 -5 -4 -3 -2 -1  0  1  2  3  4  3  3  2  1  0 -1 -2 -2 -3 -4 -4 -4 -3 -3 -3 -2 -2 -2 -1 -1 -1  0  0  0  0 ];
 
 for (j=2:size(xtarl,2))
     x_tar = xtarl(j);
     y_tar = ytarl(j);
-    for(i=1:200)
+    
+    if x_tar > 9
+        pause(.1);
+    end
+    
+    %for(i=1:200)
         start_time = tic;
-        theta_des = atan2(y_tar - y, x_tar-x);
+        theta_des = atan2(ytarl(j) - y, xtarl(j)-x);
         path_pts = [xtarl(j-1) ytarl(j-1); xtarl(j) ytarl(j)]; 
         w = w_PID(theta, theta_des, x, y, path_pts);
         
@@ -40,8 +51,8 @@ for (j=2:size(xtarl,2))
 %             wnoisemax = .3;
 %             velnoisemax = .1;  % variance of vel
 %         end
-      velnoisemax =0;
-      wnoisemax =0;
+      velnoisemax =0.1;
+      wnoisemax =0.1;
       
       if (w > 3)
           w = 3;
@@ -56,11 +67,11 @@ for (j=2:size(xtarl,2))
         y =X(end,2);
         
         % add errors
-        theta = theta + .1;
+    %    theta = theta + .1;
         SavePIDdata(x,y, theta, vel, w, xtarl(j-1), ytarl(j-1),xtarl(j), ytarl(j), theta_des, 6);
         elap_time = toc(start_time);
         pause(.1 - elap_time);
-    end
+  %  end
     
 %     y_tar = rand*20.0 - 10.0;
 %     x_tar = rand*20.0 - 10.0;
