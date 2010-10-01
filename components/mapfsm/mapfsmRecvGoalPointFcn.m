527499d9c15444e536cd542d26afa7338f5268a7
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-10-01 14:32:21 +0000

    mapfsm can now take a whole path as the goal point for goToPoint
    git-svn-id: svn://localhost:5050/magic2010/trunk@514 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/mapfsm/mapfsmRecvGoalPointFcn.m
+++ components/mapfsm/mapfsmRecvGoalPointFcn.m
@@ -4,6 +4,6 @@ global MP PATH_DATA MPOSE
 
 if ~isempty(data)
   goal = deserialize(data);
-  PATH_DATA.goToPointGoal = [goal(1) goal(2) atan2(goal(2)-MPOSE.y,goal(1)-MPOSE.x)];
+  PATH_DATA.goToPointGoal = [goal(:,1) goal(:,2) atan2(goal(2)-MPOSE.y,goal(1)-MPOSE.x)*ones(size(goal,1),1)];
   MP.sm = setEvent(MP.sm, 'goToPoint');
 end
