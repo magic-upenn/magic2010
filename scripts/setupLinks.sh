23a9d32f1fd60f2bbcda67246998cdefd5dcc115
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-10-05 17:54:36 +0000

    updating setup links
    git-svn-id: svn://localhost:5050/magic2010/trunk@563 61389559-6c2a-4883-acdc-2d27d9f19ada

--- scripts/setupLinks.sh
+++ scripts/setupLinks.sh
@@ -2,6 +2,11 @@ sudo ln -s /opt/matlab/bin/matlab /usr/local/bin/matlab
 sudo ln -s /opt/matlab/bin/mex /usr/local/bin/mex
 sudo ln -s /opt/matlab/bin/mexext /usr/local/bin/mexext
 
+sudo rm /usr/local/include/ipc.h
+sudo rm /usr/local/lib/libipc.a
+sudo rm /usr/local/bin/central
+sudo rm /usr/local/bin/xdrgen
+
 sudo ln -s $MAGIC_DIR/ipc/ipc.h /usr/local/include/ipc.h
 sudo ln -s $MAGIC_DIR/ipc/libipc.a.linux32 /usr/local/lib/libipc.a
 sudo ln -s $MAGIC_DIR/ipc/central.linux32 /usr/local/bin/central
