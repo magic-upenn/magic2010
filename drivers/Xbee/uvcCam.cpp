b1ecb4598a9547718e6ef9a02d5bee2f12412faa
akushley <akushley@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-10-06 08:28:33 +0000

    minor cleanup of video driver
    git-svn-id: svn://localhost:5050/magic2010/trunk@574 61389559-6c2a-4883-acdc-2d27d9f19ada

--- drivers/Xbee/uvcCam.cpp
+++ drivers/Xbee/uvcCam.cpp
@@ -17,10 +17,14 @@
 #define HEIGHT 240
 #define NBUFFERS 2
 
+using namespace std;
+
 mxArray *bufArray = NULL;
-bool initMexfile = false;
-bool initV4l2    = false;
-bool streamOn    = false;
+bool init         = false;
+bool streamOn     = false;
+int _width         = WIDTH;
+int _height        = HEIGHT;
+string dev        = string(VIDEO_DEVICE);
 
 void mexExit(void)
 {
@@ -38,32 +42,76 @@ void mexExit(void)
 */
     //instead of destroying the array, re-assign it to a dummy matrix
     //this memory will be free'd when the matlab's variable is released from memory
-    bufArray = mxCreateNumericMatrix(WIDTH/2, HEIGHT, mxUINT32_CLASS, mxREAL);
+    bufArray = mxCreateNumericMatrix(_width/2, _height, mxUINT32_CLASS, mxREAL);
   }
+}
+
+string GetString(const mxArray * mxStr)
+{
+  char * strChar = mxArrayToString(mxStr);
+  if (strChar)
+  {
+    string ret = string(strChar);
+    mxFree(strChar);
+    return ret;
+  }
+  else
+  {
+    string ret = string("");
+    return ret;
+  }
+}
+
+void CheckConnection()
+{
+  if (!init)
+    mexErrMsgTxt("not initialized");
 }
 
 void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
 {
-  if (!initMexfile) {
-    printf("opening video device..."); fflush(stdout);
-    v4l2_open(VIDEO_DEVICE);
-
-    bufArray = mxCreateNumericMatrix(WIDTH/2, HEIGHT, mxUINT32_CLASS, mxREAL);
-    mexMakeArrayPersistent(bufArray);
-    mxFree(mxGetData(bufArray));
-    initMexfile = true;
-    mexAtExit(mexExit);
-    printf("done\n");
-  }
-  
   // Get input arguments
   if (nrhs == 0) {
     mexErrMsgTxt("Need input argument");
     return;
   }
 
-  std::string cmd = mxArrayToString(prhs[0]);
-  if (cmd == "read") {
+  string cmd = GetString(prhs[0]);
+
+  if (cmd == "init")
+  {
+    if (!init)
+    {
+      if (nrhs>1) dev = GetString(prhs[1]);
+      if (nrhs>2)
+      {
+        _width  = mxGetPr(prhs[2])[0];
+        _height = mxGetPr(prhs[2])[1];
+
+        if (_width < 0 || _height < 0 || _width > 10000 || _height > 10000)
+          mexErrMsgTxt("bad _width or _height");
+      }
+
+      printf("opening video device %s ...",dev.c_str()); fflush(stdout);
+      if (v4l2_open(dev.c_str()))
+        mexErrMsgTxt("could not open device");
+
+      bufArray = mxCreateNumericMatrix(_width/2, _height, mxUINT32_CLASS, mxREAL);
+      mexMakeArrayPersistent(bufArray);
+      mxFree(mxGetData(bufArray));
+      
+      if (v4l2_init())
+        mexErrMsgTxt("could not initialize device");
+
+      mexAtExit(mexExit);
+      init = true;
+      printf("done\n");
+    }
+  }
+  
+  else if (cmd == "read")
+  {
+    CheckConnection();
     int ibuf = v4l2_read_frame();
     if (ibuf >= 0) {
       mxSetData(bufArray, v4l2_get_buffer(ibuf, NULL));
@@ -75,33 +123,36 @@ void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
       return;
     }
   }
-  else if (cmd == "get_ctrl") {
+  else if (cmd == "get_ctrl")
+  {
+    CheckConnection();
     char *key = mxArrayToString(prhs[1]);
     int value;
     int ret = v4l2_get_ctrl(key, &value);
     plhs[0] = mxCreateDoubleScalar(value);
+    if (key) mxFree(key);
     return;
   }
-  else if (cmd == "set_ctrl") {
+  else if (cmd == "set_ctrl")
+  {
+    CheckConnection();
     char *key = mxArrayToString(prhs[1]);
     int value = mxGetScalar(prhs[2]);
     int ret = v4l2_set_ctrl(key, value);
     plhs[0] = mxCreateDoubleScalar(ret);
+    if (key) mxFree(key);
     return;
   }
-  else if (cmd == "init") {
-    if (!initV4l2)
-    {
-      v4l2_init();
-      initV4l2 = true;
-    }
-  }
-  else if (cmd == "stream_on") {
+  else if (cmd == "stream_on")
+  {
+    CheckConnection();
     if (!streamOn)
       v4l2_stream_on();
     streamOn = true;
   }
-  else if (cmd == "stream_off") {
+  else if (cmd == "stream_off")
+  {
+    CheckConnection();
     if (streamOn)
       v4l2_stream_off();
     streamOn = false;
