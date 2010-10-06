8e0d770b20fa50a4831eaec0afd2acf4ac9bae68
akushley <akushley@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-10-06 19:37:19 +0000

    receive images script
    git-svn-id: svn://localhost:5050/magic2010/trunk@587 61389559-6c2a-4883-acdc-2d27d9f19ada

--- drivers/Udp/testUdpReceiveImages.m
+++ drivers/Udp/testUdpReceiveImages.m
@@ -4,8 +4,23 @@ addpath ../Xbee/
 addr = '192.168.10.19';
 port = 12345;
 
+width = 160;
+height = 120;
 UdpReceiveAPI('connect',addr,port);
 
+
+nRobots = 6;
+
+figure(1); clf(gcf);
+hs=[];
+
+for ii=1:nRobots
+  subplot(1,nRobots,ii);
+  hs(ii) = image(zeros(height,width,3,'uint8'));
+  set(gca,'ydir','normal');
+end
+
+drawnow
 
 while(1)
   packets = UdpReceiveAPI('receive');
@@ -15,7 +30,8 @@ while(1)
     for ii=1:n
       imPacket = deserialize(packets(ii).data);
       im = djpeg(imPacket.jpg);
-      image(im); set(gca,'ydir','normal'); drawnow;
+      id = imPacket.robotId;
+      set(hs(id),'cdata',im); drawnow;
     end
   end
   pause(0.01);
