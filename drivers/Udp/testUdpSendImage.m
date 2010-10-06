0eed039cf6a24cceead78c577c2bf71ba2303031
akushley <akushley@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-10-06 09:06:01 +0000

    updating camera script
    git-svn-id: svn://localhost:5050/magic2010/trunk@577 61389559-6c2a-4883-acdc-2d27d9f19ada

--- drivers/Udp/testUdpSendImage.m
+++ drivers/Udp/testUdpSendImage.m
@@ -14,12 +14,13 @@ UdpSendAPI('connect',addr,port);
 cntr = 1;
 
 
-uvcCam('init');
+uvcCam('init','/dev/video0',[800 600]);
 uvcCam('stream_on');
 uvcCam('set_ctrl','contrast', 32);
 
 id = GetRobotId();
-selectedId = -1;
+selectedId = id;
+cnt = 0;
 
 while(1)
   msgs = ipcAPI('listen',5);
@@ -33,13 +34,18 @@ while(1)
   pause(0.0300);
   imYuyv = uvcCam('read');
   if (~isempty(imYuyv)) && (id == selectedId)
-   imRgb = yuyv2rgb(imYuyv);
-   jpgc = cjpeg(imRgb);
+   cnt      = cnt + 1;
+   imRgb    = yuyv2rgb(imYuyv);
+   jpgc     = cjpeg(imRgb);
    data.jpg = jpgc;
    data.t   = GetUnixTime();
-   payload = serialize(data);
+   data.id  = 'Robot1/FrontCam';
+   data.cnt = cnt;
+   payload  = serialize(data);
+
    UdpSendAPI('send',payload);
    fprintf(1,'send packet of size %d  ',length(payload));
+   image(imRgb); drawnow;
   end
   cntr = cntr +1;
 end
