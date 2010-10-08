0ecc2063bd75b9b2971f49b67aa14b57e9c96490
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-10-08 18:09:02 +0000

    wider servo range so we can see in front of our wheels better
    git-svn-id: svn://localhost:5050/magic2010/trunk@605 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/examples/sendServoStart.m
+++ components/examples/sendServoStart.m
@@ -8,8 +8,8 @@ ipcAPIDefine(servoMsgName,MagicServoControllerCmdSerializer('getFormat'));
 
 servoCmd.id           = 1;
 servoCmd.mode         = 3; %0: disabled; 1: feedback only: 2: point minAngle is the goal), 3: servo mode
-servoCmd.minAngle     = -35;
-servoCmd.maxAngle     = 35;
+servoCmd.minAngle     = -47;
+servoCmd.maxAngle     = 47;
 servoCmd.speed        = 70;
 servoCmd.acceleration = 300;
 
--- components/mapfsm/sLook.m
+++ components/mapfsm/sLook.m
@@ -31,9 +31,9 @@ switch event
  case 'exit'
   servoCmd.id           = 1;
   servoCmd.mode         = 3; %0 for point mode (minAngle is the goal), 1 for servo mode
-  servoCmd.minAngle     = -35;
-  servoCmd.maxAngle     = 35;
-  servoCmd.speed        = 100;
+  servoCmd.minAngle     = -47;
+  servoCmd.maxAngle     = 47;
+  servoCmd.speed        = 70;
   servoCmd.acceleration = 300;
 
   content = MagicServoControllerCmdSerializer('serialize',servoCmd);
