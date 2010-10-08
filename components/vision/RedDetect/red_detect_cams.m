a206fd5d4aa5095248ab8e9a53ed6395b1fbbd7a
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-10-08 19:33:59 +0000

    codyp: sped up yuyv2rgb, adjusted omni params
    git-svn-id: svn://localhost:5050/magic2010/trunk@610 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/vision/RedDetect/red_detect_cams.m
+++ components/vision/RedDetect/red_detect_cams.m
@@ -1,9 +1,9 @@
 function [omni_sm, front_sm, omni_cands, front_cands, omni_stats, front_stats] = red_detect_cams()
 	tic
-	omni  = get_image(0,3); 
+	omni  = get_image(0,1); 
 	front = get_image(1,3);
-	circle = [600,800,650]/2;
-	front = imrotate(front,180); 
+	circle = [600,800,650];
+	front = imrotate(front,180);
 	omni_flat = linear_unroll(omni,circle(2),circle(1),circle(3));
 	[red,omni_stats] = find_red_candidates(omni_flat); 
 	[red,front_stats] = find_red_candidates(front);
