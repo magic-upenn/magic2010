eadfca1788eac833c3d4cfa325fceb595faa6d60
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-10-11 02:05:00 +0000

    servo range in mapfsm
    git-svn-id: svn://localhost:5050/magic2010/trunk@620 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/mapfsm/sLook.m
+++ components/mapfsm/sLook.m
@@ -31,8 +31,8 @@ switch event
  case 'exit'
   servoCmd.id           = 1;
   servoCmd.mode         = 3; %0 for point mode (minAngle is the goal), 1 for servo mode
-  servoCmd.minAngle     = -47;
-  servoCmd.maxAngle     = 47;
+  servoCmd.minAngle     = -40;
+  servoCmd.maxAngle     = 40;
   servoCmd.speed        = 70;
   servoCmd.acceleration = 300;
 
