f54cab35a9fbbb1e75880d16ecdbabb47f60c440
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-10-11 01:56:39 +0000

    cutting down the servo range - it is picking up the wheels
    git-svn-id: svn://localhost:5050/magic2010/trunk@619 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/examples/sendServoStart.m
+++ components/examples/sendServoStart.m
@@ -8,8 +8,8 @@ ipcAPIDefine(servoMsgName,MagicServoControllerCmdSerializer('getFormat'));
 
 servoCmd.id           = 1;
 servoCmd.mode         = 3; %0: disabled; 1: feedback only: 2: point minAngle is the goal), 3: servo mode
-servoCmd.minAngle     = -47;
-servoCmd.maxAngle     = 47;
+servoCmd.minAngle     = -40;
+servoCmd.maxAngle     = 40;
 servoCmd.speed        = 70;
 servoCmd.acceleration = 300;
 
