5f1b67a02a4b3fa1c39dcf2c2b04ddca6a9e3c60
jbutzke <jbutzke@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-10-31 00:10:28 +0000

    [JMB] add full map support to OutputMap.m
    git-svn-id: svn://localhost:5050/magic2010/trunk@755 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/gcs/map/OutputMap.m
+++ components/gcs/map/OutputMap.m
@@ -91,8 +91,8 @@ end
 
 for ID = 1:size(OOI_PATH,2)
     if ~isempty(OOI_PATH(ID))
-        OPATH(ID).x = floor((OOI_PATH(ID).x + mapEoffset)/res);
-        OPATH(ID).y = floor((OOI_PATH(ID).y + mapNoffset)/res);
+        OPATH(ID).x = floor((OOI_PATH(ID).x -MAGIC_CONSTANTS.mapEastMin)/res);
+        OPATH(ID).y = floor((OOI_PATH(ID).y -MAGIC_CONSTANTS.mapNorthMin)/res);
     else
         OPATH(ID) = [];
 %         OPATH(ID).y = [];
@@ -101,18 +101,130 @@ end
 
 for ID =1:size(NC_PATH, 2)
     if ~isempty(NC_PATH(ID))
-        NPATH(ID).x = floor((NC_PATH(ID).x + mapEoffset)/res);
-        NPATH(ID).y = floor((NC_PATH(ID).y + mapNoffset)/res);
+        NPATH(ID).x = floor((NC_PATH(ID).x -MAGIC_CONSTANTS.mapEastMin)/res);
+        NPATH(ID).y = floor((NC_PATH(ID).y -MAGIC_CONSTANTS.mapNorthMin)/res);
     else
         NPATH(ID) = [];
 %         NPATH(ID).y = [];
     end
 end
 
+
+% print a full map
+minx = 1; miny =1;
+maxx = xdim; maxy = ydim;
+
+  % setup the plot map
+        plot_map = map(minx:maxx, miny:maxy);
+        plot_map = vertcat(zeros(HEADERSPACE,ydim), plot_map);
+        
+        %setup the output map
+        out_map1(:,:) = repmat(CDUNK(1), xdim+HEADERSPACE, ydim);
+        out_map2(:,:) = repmat(CDUNK(2), xdim+HEADERSPACE, ydim);
+        out_map3(:,:) = repmat(CDUNK(3), xdim+HEADERSPACE, ydim);
+        
+        %make grid background
+        for gx= 0:(xdim*res -1)
+            for gy=0:(ydim*res -1)
+                if (mod(gx+gy, 2))
+                    out_map1((gx/res)+1:((gx+1)/res), (gy/res)+1:((gy+1)/res)) = CLUNK(1);
+                    out_map2((gx/res)+1:((gx+1)/res), (gy/res)+1:((gy+1)/res)) = CLUNK(2);
+                    out_map3((gx/res)+1:((gx+1)/res), (gy/res)+1:((gy+1)/res)) = CLUNK(3);
+                end
+            end
+        end
+        
+        % plot the known
+        pmidx = plot_map < 0;
+        out_map1(pmidx) = (-CFREEDEL(1).*plot_map(pmidx)/100)+CFREEMIN(1);
+        out_map2(pmidx) = (-CFREEDEL(2).*plot_map(pmidx)/100)+CFREEMIN(2);
+        out_map3(pmidx) = (-CFREEDEL(3).*plot_map(pmidx)/100)+CFREEMIN(3);
+        
+        %plot the presumed free by movement
+        pmidx = plot_map > 2;
+        out_map1(pmidx) = (CFREEDEL(1).*plot_map(pmidx)/100)+CFREEMIN(1);
+        out_map2(pmidx) = (CFREEDEL(2).*plot_map(pmidx)/100)+CFREEMIN(2);
+        out_map3(pmidx) = (CFREEDEL(3).*plot_map(pmidx)/100)+CFREEMIN(3);
+        
+        %plot the walls
+        pmidx = plot_map >=90;
+        out_map1(pmidx) = CWALL(1);
+        out_map2(pmidx) = CWALL(2);
+        out_map3(pmidx) = CWALL(3);
+        
+        out_map = cat(3, out_map1, out_map2, out_map3);
+        
+        clf(h); hold off; image(out_map); hold on; axis equal; axis tight;
+        
+        %add the filename
+        text(10, 5, [printname '\_full'], 'Color', CFILE, 'FontSize', 10);
+        
+        % scale
+        plot([10 10], [10.5 20.5], 'Color', CSCALE);
+        plot([9 11], [10.5 10.5], 'Color', CSCALE);
+        plot([9 11], [20.5 20.5], 'Color', CSCALE);
+        text(12, 15, '1m', 'Color', CSCALE, 'FontSize', 8);
+        
+        % orientation
+        plot([35 35 30], [13 18 18], 'Color', CSCALE);
+        text(36, 13, 'x', 'Color', CSCALE, 'FontSize', 6);
+        text(30, 16, 'y', 'Color', CSCALE, 'FontSize', 6);
+        
+        % robot start
+        for ID = 1:size(RPATH,2)
+           if ~isempty(RPATH(ID).x)
+                            patch(RPATH(ID).y(1)+[-1 0 1]-miny, RPATH(ID).x(1)+[1 -2 1]-minx+HEADERSPACE, CSTART);
+                       end
+        end
+        
+        % plot the robot, OOI and NC paths
+        for ID =1:size(RPATH,2)
+            if ~isempty(RPATH(ID).x)
+                    plot(RPATH(ID).y-miny, RPATH(ID).x- minx+HEADERSPACE, 'Color', CRPATH, 'LineWidth', .02);
+            end
+        end
+        
+        for ID = 1:size(OPATH,2)
+            if ~isempty(OPATH(ID))
+                    plot(OPATH(ID).y-miny, OPATH(ID).x- minx +HEADERSPACE, 'Color', COOIPATH, 'LineWidth', .02);
+            end
+        end
+        
+        for ID =1:size(NPATH,2)
+            if ~isempty(NPATH(ID))
+                    plot(NPATH(ID).y-miny, NPATH(ID).x - minx +HEADERSPACE, 'Color', CNCPATH, 'LineWidth', .02);
+            end
+            end
+        
+        % OOI info
+        for oidx=1: size(OOI,2)
+            xx = (OOI(oidx).x + mapEoffset)/res;
+            yy = (OOI(oidx).y + mapNoffset)/res;
+            if ((xx>=minx) && (xx <=maxx) && (yy>=miny) && (yy<=maxy))
+                patch(circ_y+yy-miny, circ_x+xx-minx, COOI);
+                text(yy-miny-.4, xx-minx, OOI(oidx).serial, 'Color', COOITEXT, 'FontSize', 8);
+            end
+        end
+                
+         
+        
+        
+        set(gca, 'Position', [.01 .01 .98 .98], 'Visible', 'off', 'ActivePositionProperty', 'Position');
+        orient tall
+        drawnow;
+        %output the file
+        print(h, '-dtiff',  [filename '_full.tif']);
+
+
+
+
+
  for x=1:num_x
      for y=1:num_y
 %x =3; y=8;
-
+% out_map1 = [];
+% out_map2 = [];
+% out_map3 = [];
 % calc endpoints of map
                 minx = (x-1)*MAPSIZE/res +1;
         maxx = (x*MAPSIZE)/res;
@@ -124,9 +236,9 @@ end
         plot_map = vertcat(zeros(HEADERSPACE,200), plot_map);
         
         %setup the output map
-        out_map1(:,:) = repmat(CDUNK(1), MAPSIZE/res+HEADERSPACE, MAPSIZE/res);
-        out_map2(:,:) = repmat(CDUNK(2), MAPSIZE/res+HEADERSPACE, MAPSIZE/res);
-        out_map3(:,:) = repmat(CDUNK(3), MAPSIZE/res+HEADERSPACE, MAPSIZE/res);
+        out_map1 = repmat(CDUNK(1), MAPSIZE/res+HEADERSPACE, MAPSIZE/res);
+        out_map2 = repmat(CDUNK(2), MAPSIZE/res+HEADERSPACE, MAPSIZE/res);
+        out_map3 = repmat(CDUNK(3), MAPSIZE/res+HEADERSPACE, MAPSIZE/res);
         
         %make grid background
         for gx= 0:(MAPSIZE+HEADERSPACE*res -1)
