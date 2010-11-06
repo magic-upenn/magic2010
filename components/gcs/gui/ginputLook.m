d8dd30b4a17d8eef0a48cd5a8a23530702257053
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-11-06 01:01:47 +0000

    gcs sends a tilt angle of zero when sending a look
    git-svn-id: svn://localhost:5050/magic2010/trunk@803 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/gcs/gui/ginputLook.m
+++ components/gcs/gui/ginputLook.m
@@ -9,6 +9,7 @@ msgName = ['Robot',num2str(id),'/Look_Msg'];
 if ~isempty(xp) && HAVE_ROBOTS,
   %ooi = [xp(1) yp(1)];
   ooi.theta = atan2(yp(1)-RPOSE{id}.y,xp(1)-RPOSE{id}.x);
+  ooi.phi = 0;
   ooi.type = 'look';
   try
     ROBOTS(id).ipcAPI('publish', msgName, serialize(ooi));
