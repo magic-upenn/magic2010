c8309be02de9f818565ccf47d966d387754067f9
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2010-11-06 20:51:08 +0000

    Alex: putting in more sanity checks into slam
    git-svn-id: svn://localhost:5050/magic2010/trunk@819 61389559-6c2a-4883-acdc-2d27d9f19ada

--- components/common/imuInit.m
+++ components/common/imuInit.m
@@ -9,5 +9,8 @@ if isempty(IMU) || ~isfield(IMU,'initialized') || (IMU.initialized ~= 1)
   ipcAPIDefine(IMU.msgName,MagicImuFilteredSerializer('getFormat'));
   
   IMU.initialized = 1;
+  %IMU.initDelta   = []; %time between first imu packettimestamp and unix time at the moment of reception
+  IMU.tLastArrival       = []; %time of arrival of last packet
+  IMU.timeout = 0.1;
   disp('Imu initialized');
 end
--- components/common/ipcRecvImuFcn.m
+++ components/common/ipcRecvImuFcn.m
@@ -5,3 +5,5 @@ global IMU
 if ~isempty(data)
   IMU.data = MagicImuFilteredSerializer('deserialize',data);
 end
+
+IMU.tLastArrival = GetUnixTime();
