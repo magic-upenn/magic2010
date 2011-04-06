48574cb16c073596a56bd66b112cddc5008fe458
akushley <akushley@61389559-6c2a-4883-acdc-2d27d9f19ada>
2011-04-06 00:37:42 +0000

    latest params
    git-svn-id: svn://localhost:5050/magic2010/trunk@895 61389559-6c2a-4883-acdc-2d27d9f19ada

--- drivers/MicroGateway/MicroParams.cc
+++ drivers/MicroGateway/MicroParams.cc
@@ -19,12 +19,12 @@ int MicroParamsInitialize()
 
   ptable1.id           = 1;
   ptable1.mode         = 0;
-  ptable1.accBiasX     = 660;
-  ptable1.accBiasY     = 660;
-  ptable1.accBiasZ     = 660;
-  ptable1.accSenX      = (1.0/139.0);
-  ptable1.accSenY      = (1.0/139.0);
-  ptable1.accSenZ      = (1.0/139.0);
+  ptable1.accBiasX     = 663;
+  ptable1.accBiasY     = 653;
+  ptable1.accBiasZ     = 670;
+  ptable1.accSenX      = (1.0/133.0);
+  ptable1.accSenY      = (1.0/133.0);
+  ptable1.accSenZ      = (1.0/133.0);
   ptable1.gyroSenX     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable1.gyroSenY     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable1.gyroSenZ     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
@@ -37,12 +37,12 @@ int MicroParamsInitialize()
 
   ptable2.id           = 2;
   ptable2.mode         = 0;
-  ptable2.accBiasX     = 660;
-  ptable2.accBiasY     = 660;
-  ptable2.accBiasZ     = 660;
-  ptable2.accSenX      = (1.0/139.0);
-  ptable2.accSenY      = (1.0/139.0);
-  ptable2.accSenZ      = (1.0/139.0);
+  ptable2.accBiasX     = 652;
+  ptable2.accBiasY     = 644;
+  ptable2.accBiasZ     = 645;
+  ptable2.accSenX      = (1.0/134.0);
+  ptable2.accSenY      = (1.0/133.0);
+  ptable2.accSenZ      = (1.0/133.0);
   ptable2.gyroSenX     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable2.gyroSenY     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable2.gyroSenZ     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
@@ -55,12 +55,12 @@ int MicroParamsInitialize()
 
   ptable3.id           = 3;
   ptable3.mode         = 0;
-  ptable3.accBiasX     = 660;
-  ptable3.accBiasY     = 660;
-  ptable3.accBiasZ     = 660;
+  ptable3.accBiasX     = 662;
+  ptable3.accBiasY     = 670;
+  ptable3.accBiasZ     = 680;
   ptable3.accSenX      = (1.0/139.0);
   ptable3.accSenY      = (1.0/139.0);
-  ptable3.accSenZ      = (1.0/139.0);
+  ptable3.accSenZ      = (1.0/131.0);
   ptable3.gyroSenX     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable3.gyroSenY     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable3.gyroSenZ     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
@@ -74,12 +74,12 @@ int MicroParamsInitialize()
 
   ptable4.id           = 4;
   ptable4.mode         = 0;
-  ptable4.accBiasX     = 660;
-  ptable4.accBiasY     = 660;
+  ptable4.accBiasX     = 670;
+  ptable4.accBiasY     = 650;
   ptable4.accBiasZ     = 660;
-  ptable4.accSenX      = (1.0/139.0);
-  ptable4.accSenY      = (1.0/139.0);
-  ptable4.accSenZ      = (1.0/139.0);
+  ptable4.accSenX      = (1.0/134.0);
+  ptable4.accSenY      = (1.0/135.0);
+  ptable4.accSenZ      = (1.0/134.0);
   ptable4.gyroSenX     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable4.gyroSenY     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable4.gyroSenZ     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
@@ -92,12 +92,12 @@ int MicroParamsInitialize()
 
   ptable5.id           = 5;
   ptable5.mode         = 0;
-  ptable5.accBiasX     = 660;
-  ptable5.accBiasY     = 660;
-  ptable5.accBiasZ     = 660;
-  ptable5.accSenX      = (1.0/139.0);
-  ptable5.accSenY      = (1.0/139.0);
-  ptable5.accSenZ      = (1.0/139.0);
+  ptable5.accBiasX     = 658;
+  ptable5.accBiasY     = 661;
+  ptable5.accBiasZ     = 675;
+  ptable5.accSenX      = (1.0/132.0);
+  ptable5.accSenY      = (1.0/136.0);
+  ptable5.accSenZ      = (1.0/133.0);
   ptable5.gyroSenX     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable5.gyroSenY     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable5.gyroSenZ     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
@@ -111,12 +111,12 @@ int MicroParamsInitialize()
 
   ptable6.id           = 6;
   ptable6.mode         = 0;
-  ptable6.accBiasX     = 660;
-  ptable6.accBiasY     = 660;
-  ptable6.accBiasZ     = 660;
-  ptable6.accSenX      = (1.0/139.0);
-  ptable6.accSenY      = (1.0/139.0);
-  ptable6.accSenZ      = (1.0/139.0);
+  ptable6.accBiasX     = 657;
+  ptable6.accBiasY     = 662;
+  ptable6.accBiasZ     = 675;
+  ptable6.accSenX      = (1.0/133.0);
+  ptable6.accSenY      = (1.0/133.0);
+  ptable6.accSenZ      = (1.0/133.0);
   ptable6.gyroSenX     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable6.gyroSenY     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable6.gyroSenZ     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
@@ -166,12 +166,12 @@ int MicroParamsInitialize()
 
   ptable9.id           = 9;
   ptable9.mode         = 0;
-  ptable9.accBiasX     = 660;
-  ptable9.accBiasY     = 660;
-  ptable9.accBiasZ     = 660;
-  ptable9.accSenX      = (1.0/139.0);
-  ptable9.accSenY      = (1.0/139.0);
-  ptable9.accSenZ      = (1.0/139.0);
+  ptable9.accBiasX     = 681;
+  ptable9.accBiasY     = 677;
+  ptable9.accBiasZ     = 683;
+  ptable9.accSenX      = (1.0/135.0);
+  ptable9.accSenY      = (1.0/135.0);
+  ptable9.accSenZ      = (1.0/135.0);
   ptable9.gyroSenX     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable9.gyroSenY     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
   ptable9.gyroSenZ     = ADC_VOLTAGE_MV/1023.0*(M_PI/180.0/3.5);
