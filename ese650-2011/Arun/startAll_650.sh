c3dba1ddd50ab2d702a96f4a9e197db39e5c322f
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2011-04-13 02:11:47 +0000

    git-svn-id: svn://localhost:5050/magic2010/trunk@908 61389559-6c2a-4883-acdc-2d27d9f19ada

--- ese650-2011/Arun/startAll_650.sh
+++ ese650-2011/Arun/startAll_650.sh
@@ -41,7 +41,6 @@ StartFunction ()
 # red: matlab -nodesktop -r startRed
 StartFunction <<EOF
 central:nice -n -5 central -s
-spread:/usr/local/sbin/spread -l y
 mg:nice -n -5 $MAGIC_DIR/drivers/MicroGateway/runMicroGateway2 /dev/ttyUSB0
 frontcam: matlab -nodesktop -r startFrontCam
 omnicam: matlab -nodesktop -r startOmniCam
