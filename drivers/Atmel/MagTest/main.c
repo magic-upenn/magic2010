a774134c060484d248293ef8c28a8249f401c9bf
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2011-04-18 19:09:12 +0000

    Changed packet format
    git-svn-id: svn://localhost:5050/magic2010/trunk@913 61389559-6c2a-4883-acdc-2d27d9f19ada

--- drivers/Atmel/MagTest/main.c
+++ drivers/Atmel/MagTest/main.c
@@ -38,20 +38,28 @@ int main(void)
 
   int ret;
   uint16_t magData[4];
-
+  uint8_t buf[12];
+  buf[0] = 0x55;
+  buf[1] = 0x55;
+  buf[10] = 0xAA;
+  buf[11] = 0xAA;
   while(1)
   {
     ret = MagGetData(magData);
     if (ret == 0)
     {
-      uart0_printf("*** %d %d %d %d\r\n",magData[0],magData[1],magData[2],magData[3]);      
-      _delay_ms(50);
+      buf[2] = magData[0] >> 8;
+      buf[3] = magData[0] & 0xFF;
+      buf[4] = magData[1] >> 8;
+      buf[5] = magData[1] & 0xFF;
+      buf[6] = magData[2] >> 8;
+      buf[7] = magData[2] & 0xFF;
+      buf[8] = magData[3] >> 8;
+      buf[9] = magData[3] & 0xFF;
+      uart0_putdata(buf, 12); // takes ~ 1ms (115200bps)
+      _delay_ms(19);
     }
-
-    _delay_ms(2);
-    //uart0_putchar('.');
   }  
-  
 
   return 0;
 }
