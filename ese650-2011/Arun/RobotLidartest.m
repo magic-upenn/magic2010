a73414a0fe202dce884de1b65f5299d87c9f0f1d
magic <magic@61389559-6c2a-4883-acdc-2d27d9f19ada>
2011-05-01 23:09:57 +0000

    git-svn-id: svn://localhost:5050/magic2010/trunk@942 61389559-6c2a-4883-acdc-2d27d9f19ada

---
+++ ese650-2011/Arun/RobotLidartest.m
@@ -0,0 +1,151 @@
+clear all;
+close all;
+SetMagicPaths;
+%lidar0MsgName = GetMsgName('Lidar0');
+
+robotId = '5';
+LidarMsgName = ['Robot' robotId '/Lidar0'];    
+imuMsgName = ['Robot' robotId '/ImuFiltered']; % IMU
+ServoMsgName = ['Robot' robotId '/Servo1']; 
+ipcAPIConnect('localhost');
+ipcAPISubscribe(LidarMsgName);
+ipcAPISubscribe(ServoMsgName);
+ipcAPISubscribe(imuMsgName);
+
+Lidar ={}; 
+Servo = {};
+Imu = {};
+
+timeout = 25;
+
+% Initialize stuff
+k_Imu = 1;
+k_Lidar = 1;
+k_Servo = 1;
+
+pitch = 0;
+roll = 0;
+yaw = 0;
+servo_angl = 0;
+
+
+pts_3D = [];
+las_angles = [];
+T_servotobody = trans([0 0 0]);
+T_senstoservo = trans([0 0 0]);
+
+%% Initialize the cost map
+%global MAP
+MAP.res   = 0.05; %meters
+
+MAP.xmin  = -5;  %meters
+MAP.ymin  = -5;
+MAP.xmax  =  5;
+MAP.ymax  =  5;
+
+%dimensions of the map
+MAP.sizex  = ceil((MAP.xmax - MAP.xmin) / MAP.res + 1); %cells
+MAP.sizey  = ceil((MAP.ymax - MAP.ymin) / MAP.res + 1);
+
+MAP.map = zeros(MAP.sizex,MAP.sizey,'int8');
+
+tic;
+
+while(1)
+    if(toc > timeout)
+      break;
+    end
+    msgs = ipcAPIReceive(10);
+    len = length(msgs);
+    if len > 0
+        %disp('receiving...');
+        for i=1:len
+            switch(msgs(i).name)
+                case LidarMsgName
+                    lidarScan =  MagicLidarScanSerializer('deserialize',msgs(i).data);
+                    %angles = linspace(lidarScan.startAngle, lidarScan.stopAngle, length(lidarScan.ranges));
+                    %polar(angles,lidarScan.ranges,'.');
+                    Lidar{end+1} = lidarScan;
+                    Rypr = rotz(yaw)*roty(pitch)*rotx(roll); 
+                    Rsensor = roty(servo_angl)'; % transpose is the inverse of the rotation
+                    %[Lidar(k_Lidar).startAngle Lidar(k_Lidar).angleStep   Lidar(k_Lidar).stopAngle]
+                    if(isempty(las_angles))
+                        las_angles = Lidar{k_Lidar}.startAngle : Lidar{k_Lidar}.angleStep : Lidar(k_Lidar).stopAngle;
+                        zs = zeros(size(las_angles));
+                        os = ones(size(las_angles));
+                        coslas_ang = cos(las_angles);
+                        sinlas_ang = sin(las_angles);
+                    end
+                    las_ranges = Lidar{k_Lidar}.ranges;
+                    xs = (las_ranges.*coslas_ang);
+                    ys = (las_ranges.*sinlas_ang);
+
+                    valid = ((las_ranges > 0.15)&(las_ranges<40));
+
+                    X = [xs;ys;zs;os]; %[x;y;0;1]
+                    Yt = Rypr*Rsensor*X(:,valid);
+
+                    xs1 = Yt(1,:);
+                    ys1 = Yt(2,:);
+
+                    %convert from meters to cells
+                    xis = ceil((xs1 - MAP.xmin) ./ MAP.res);
+                    yis = ceil((ys1 - MAP.ymin) ./ MAP.res);
+
+                    %check the indices and populate the map
+                    indGood = (xis > 1) & (yis > 1) & (xis < MAP.sizex) & (yis < MAP.sizey);
+                    inds = sub2ind(size(MAP.map),xis(indGood),yis(indGood));
+                    MAP.map(inds) = min(MAP.map(inds)+1,100);
+
+                    if(isempty(pts_3D))
+                        % 3D points
+                        figure;
+                        A = plot3(Yt(1,:),Yt(2,:),Yt(3,:),'.');
+                        pts_3D = Yt(1:3,:);
+
+                        % Cost map
+                        figure;
+                        hold on
+                        colormap gray
+                        h = imagesc(MAP.map);
+                        axis tight;
+                        title({['MAP - Iterations:',num2str(k_Lidar)];['Pitch:',num2str(pitch)]});
+
+                    elseif(mod(k_Lidar,10) == 0)
+                        % 3D points
+                        xd = get(A,'Xdata');
+                        yd = get(A,'Ydata');
+                        zd = get(A,'Zdata');
+                        set(A,'Xdata',[xd Yt(1,:)],'Ydata',[yd Yt(2,:)],'Zdata',[zd Yt(3,:)]);
+                        pts_3D = cat(2,pts_3D,Yt(1:3,:));
+
+                        % Cost map
+                        set(h,'CData',MAP.map);
+                        title({['MAP - Iterations:',num2str(k_Lidar)];['Pitch:',num2str(pitch)]});
+
+                        drawnow;
+                    end
+                    k_Lidar = k_Lidar+1;
+                    %drawnow;
+                    fprintf(1,'.');
+                case imuMsgName
+                    Imu{end+1} = MagicImuFilteredSerializer('deserialize',msgs(i).data);
+                    pitch = Imu{k_Imu}.pitch;
+                    roll = Imu{k_Imu}.roll;
+                    yaw = 0;
+                    k_Imu = k_Imu + 1;
+                case ServoMsgName
+                    Servo{end+1} = MagicServoStateSerializer('deserialize',msgs(i).data);
+                    servo_angl = Servo{k_Servo}.position;
+                    k_Servo = k_Servo + 1;
+            end
+        end
+    end
+end
+b = datestr(clock());
+savename = strcat('Lidardata_',b(1:11),'_',b(13:end),'.mat');
+%procname = strcat('Processed_',b(1:11),'_',b(13:end),'.mat');
+save(savename,'Lidar','Servo','Imu','MAP','pts_3D');
+
+close all
+clear all
--- ese650-2011/Arun/testLidarpts.m
+++ ese650-2011/Arun/testLidarpts.m
@@ -1,7 +1,7 @@
 clear all
 close all
 %strg = 'Lidardata_30-Apr-2011_18:48:59';
-strg = 'Lidardata_30-Apr-2011_19:38:11';
+strg = 'Lidardata_30-Apr-2011_19_38_11';
 data = load(strcat(strg,'.mat'));
 Lidar = cell2mat(data.Lidar);
 Servo = cell2mat(data.Servo);
@@ -29,27 +29,86 @@ title('Plot of the servo angles over time');
 zs = zeros(size(Lidar(1).ranges));
 os = ones(size(Lidar(1).ranges));
 pts_3D = [];
+las_angles = [];
+T_servotobody = trans([0 0 0]);
+T_senstoservo = trans([0 0 0]);
+
+%% Initialize the cost map
+global MAP
+
+MAP.res   = 0.05; %meters
+
+MAP.xmin  = -5;  %meters
+MAP.ymin  = -5;
+MAP.xmax  =  5;
+MAP.ymax  =  5;
+
+
+%dimensions of the map
+MAP.sizex  = ceil((MAP.xmax - MAP.xmin) / MAP.res + 1); %cells
+MAP.sizey  = ceil((MAP.ymax - MAP.ymin) / MAP.res + 1);
+
+MAP.map = zeros(MAP.sizex,MAP.sizey,'int8');
+
 
 for k = 1:numel(ts)
     if(ts(k) == 1)
-        Typr = rotz(yaw)*roty(pitch)*rotx(roll); 
-        Tsensor = roty(servo_angl)'; % transpose is the inverse of the rotation
+        Rypr = rotz(yaw)*roty(pitch)*rotx(roll); 
+        Rsensor = roty(servo_angl)'; % transpose is the inverse of the rotation
+        %[Lidar(k_Lidar).startAngle Lidar(k_Lidar).angleStep   Lidar(k_Lidar).stopAngle]
+        if(isempty(las_angles))
+            las_angles = Lidar(k_Lidar).startAngle : Lidar(k_Lidar).angleStep : Lidar(k_Lidar).stopAngle;
+            coslas_ang = cos(las_angles);
+            sinlas_ang = sin(las_angles);
+        end
+        las_ranges = Lidar(k_Lidar).ranges;
+        xs = (las_ranges.*coslas_ang);
+        ys = (las_ranges.*sinlas_ang);
         
-        las_angles = Lidar(k_Lidar).startAngle : Lidar(k_Lidar).angleStep : Lidar(k_Lidar).stopAngle;
-        xs = (Lidar(k_Lidar).ranges.*cos(las_angles));
-        ys = (Lidar(k_Lidar).ranges.*sin(las_angles));
+        valid = ((las_ranges > 0.15)&(las_ranges<40));
+        
         X = [xs;ys;zs;os]; %[x;y;0;1]
-        Yt = Typr*Tsensor*X;
+        Yt = Rypr*Rsensor*X(:,valid);
+        
+        xs1 = Yt(1,:);
+        ys1 = Yt(2,:);
+
+        %convert from meters to cells
+        xis = ceil((xs1 - MAP.xmin) ./ MAP.res);
+        yis = ceil((ys1 - MAP.ymin) ./ MAP.res);
+
+        %check the indices and populate the map
+        indGood = (xis > 1) & (yis > 1) & (xis < MAP.sizex) & (yis < MAP.sizey);
+        inds = sub2ind(size(MAP.map),xis(indGood),yis(indGood));
+        MAP.map(inds) = min(MAP.map(inds)+1,100);
+    
+        
         if(isempty(pts_3D))
+            % 3D points
             figure;
             A = plot3(Yt(1,:),Yt(2,:),Yt(3,:),'.');
             pts_3D = Yt(1:3,:);
+
+            % Cost map
+            figure;
+            hold on
+            colormap gray
+            h = imagesc(MAP.map);
+            axis tight;
+            title({['MAP - Iterations:',num2str(k_Lidar)];['Pitch:',num2str(pitch)]});
+
         elseif(mod(k_Lidar,10) == 0)
+            % 3D points
             xd = get(A,'Xdata');
             yd = get(A,'Ydata');
             zd = get(A,'Zdata');
             set(A,'Xdata',[xd Yt(1,:)],'Ydata',[yd Yt(2,:)],'Zdata',[zd Yt(3,:)]);
             pts_3D = cat(2,pts_3D,Yt(1:3,:));
+            
+            % Cost map
+            set(h,'CData',MAP.map);
+            title({['MAP - Iterations:',num2str(k_Lidar)];['Pitch:',num2str(pitch)]});
+
             drawnow;
         end
         k_Lidar = k_Lidar+1;
