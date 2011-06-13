27c5196e71dcdf561d8e36a4319153066caa31df
akushley <akushley@61389559-6c2a-4883-acdc-2d27d9f19ada>
2011-06-13 23:05:57 +0000

    updating packages installation script
    git-svn-id: svn://localhost:5050/magic2010/trunk@1024 61389559-6c2a-4883-acdc-2d27d9f19ada

--- scripts/installPackages.sh
+++ scripts/installPackages.sh
@@ -1,1 +1,1 @@
-sudo apt-get install openssh-server build-essential libxml2-dev ccache avrdude gcc-avr avr-libc guvcview
+sudo apt-get install openssh-server build-essential libxml2-dev ccache avrdude gcc-avr avr-libc guvcview xvnc4viewer gmountiso
