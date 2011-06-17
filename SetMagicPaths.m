9677e2d8f6a399a8e71e3ffe3a273c1f700e0fbf
akushley <akushley@61389559-6c2a-4883-acdc-2d27d9f19ada>
2011-06-17 00:30:35 +0000

    excluding all .svn directories from the matlab path
    git-svn-id: svn://localhost:5050/magic2010/trunk@1026 61389559-6c2a-4883-acdc-2d27d9f19ada

--- SetMagicPaths.m
+++ SetMagicPaths.m
@@ -1,4 +1,7 @@
 addpath( [ getenv('VIS_DIR') '/ipc' ] )
 addpath( [ getenv('VIS_DIR') '/Interfaces' ] )
 addpath( [ getenv('VIS_DIR') '/mexSerialization' ] )
-addpath( genpath(getenv('MAGIC_DIR')) )
+
+
+addpath( genpath_exclude(getenv('MAGIC_DIR'),'.svn') );
+%addpath( genpath(getenv('MAGIC_DIR')) )
