5deaa2d57242b07f7587736691966da079eea198
James Yang <james.yang92@gmail.com>
2013-06-05 18:56:15 -0400

    altered install script again

--- install.sh
+++ install.sh
@@ -2,5 +2,9 @@ cd ./scripts
 sudo ./installPackages.sh
 cd ../
 echo MAGIC_DIR=`dirname $0` >> ~/.bashrc
+echo ROBOT_ID=1 >> ~/.bashrc
+echo IPC_CENTRAL_INTERNAL=localhost:1381
+export IPC_CENTRAL_EXTERNAL=localhost:1382
+
 source ~/.bashrc
 make
