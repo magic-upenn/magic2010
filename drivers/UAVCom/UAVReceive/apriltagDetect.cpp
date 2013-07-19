ff03427daa3ddf41fa4181df19898ba6b6dad64b
Dumbledore <dumbledore@dumbledore-MS-7681.(none)>
2013-07-19 12:23:38 -0400

    Quad->April->IPC

--- drivers/UAVCom/UAVReceive/apriltagDetect.cpp
+++ drivers/UAVCom/UAVReceive/apriltagDetect.cpp
@@ -6,6 +6,7 @@
 #include <stdio.h>
 #include <unistd.h>
 
+//kquad communication 
 #include "udp.h"
 #include "jpeg_decompress.h"
 #include "imgproc.h"
@@ -22,11 +23,27 @@ using namespace std;
 
 //opencv
 #include "opencv2/opencv.hpp"
-
+//apriltags
 #include "AprilTags/TagDetector.h"
 #include "AprilTags/Tag36h11.h"
 
 #include <cmath>
+
+//ipc
+#include "ipc.h"
+#define APRIL_FORMAT "{ubyte, double, double, double, double ,double, double, double, double}"
+
+typedef struct AprilInfo {
+	uint8_t id;
+	double dt;
+	double x;
+	double y;
+	double z;
+	double yaw;
+	double pitch;
+	double roll;	
+	double distance;
+}AprilInfo;
 
 #ifndef PI
 const double PI = 3.1459265358979323846;
@@ -62,9 +79,9 @@ void wRo_to_euler(const Eigen::Matrix3d& wRo, double& yaw, double& pitch, double
     roll  = standardRad(atan2(wRo(0,2)*s - wRo(1,2)*c, -wRo(0,1)*s + wRo(1,1)*c));
   }
 
- void print_detection(AprilTags::TagDetection detection){
-    cout << "  Id: " << detection.id
-         << " (Hamming: " << detection.hammingDistance << ")";
+ void print_detection(AprilTags::TagDetection detection, AprilInfo* info){
+//	cout << "  Id: " << detection.id
+  //       << " (Hamming: " << detection.hammingDistance << ")";
 
     // recovering the relative pose of a tag:
 
@@ -86,7 +103,6 @@ void wRo_to_euler(const Eigen::Matrix3d& wRo, double& yaw, double& pitch, double
     Eigen::Matrix3d rotation;
     detection.getRelativeTranslationRotation(m_tagSize, m_fx, m_fy, m_px, m_py,
                                              translation, rotation);
-
     Eigen::Matrix3d F;
     F <<
       1, 0,  0,
@@ -95,7 +111,7 @@ void wRo_to_euler(const Eigen::Matrix3d& wRo, double& yaw, double& pitch, double
     Eigen::Matrix3d fixed_rot = F*rotation;
     double yaw, pitch, roll;
     wRo_to_euler(fixed_rot, yaw, pitch, roll);
-
+/*
     cout << "  distance=" << translation.norm()
          << "m, x=" << translation(0)
          << ", y=" << translation(1)
@@ -104,12 +120,36 @@ void wRo_to_euler(const Eigen::Matrix3d& wRo, double& yaw, double& pitch, double
          << ", pitch=" << pitch
          << ", roll=" << roll
          << endl;
+    */
+    //prepare info for ipc
+    info->id=(uint8_t)detection.id;
+    info->dt=0;
+    info->x=translation(0);
+    info->y=translation(1);
+    info->z=translation(2);
+    info->yaw=yaw;
+    info->pitch=pitch;
+    info->roll=roll;
+    info->distance=translation.norm();
 
+    //publish to IPC
+    if(IPC_publishData("Quad1/AprilInfo",&info) != IPC_OK)
+    {
+         printf("Error publishing\n");
+	 exit(1);
+    }
     // Also note that for SLAM/multi-view application it is better to
     // use reprojection error of corner points, because the noise in
     // this relative pose is very non-Gaussian; see iSAM source code
     // for suitable factors.
   }
+
+void AprilInfoHandler(MSG_INSTANCE msgRef, BYTE_ARRAY callData, void *clientData) {
+	AprilInfo *info = (AprilInfo*)callData;
+	printf("got april info:");
+	printf("id=%d, dist=%f, x=%f, y=%f, z=%f, yaw=%f, pitch=%f, roll=%f, dt=%f\n", info->id, info->distance, info->x, info->y, info->z, info->yaw, info->pitch, info->roll, info->dt);
+	IPC_freeByteArray(callData);
+}
 
 int main(int argc, char* argv[])
 {
@@ -130,15 +170,31 @@ int main(int argc, char* argv[])
        const char* window_name = "Quad-April Test";
        cv::namedWindow( window_name, CV_WINDOW_AUTOSIZE);
 
+	//set up IPC 
+	AprilInfo info;
+	IPC_setVerbosity(IPC_Print_Errors);
+        if (IPC_connectModule("Quad1/AprilInfo",NULL) != IPC_OK) {
+                printf("Error connecting to IPC\n");
+                exit(1);
+        }
+	if (IPC_defineMsg("Quad1/AprilInfo",IPC_VARIABLE_LENGTH,APRIL_FORMAT) != IPC_OK) {
+                printf("ERROR defining message\n");
+                exit(1);
+        }
+	if (IPC_subscribeData("Quad1/AprilInfo",AprilInfoHandler,NULL) != IPC_OK) {
+                printf("Error subscribing\n");
+                exit(1);
+        }
+
+
   	while(1)
  	 {
+	  IPC_listen(0);
   	  UdpReceiveGetPackets(udp_packets);
-          if(udp_packets.end() == udp_packets.begin())
+          if(udp_packets.end() != udp_packets.begin())
           {
-              // printf("no packets!\n");
-          }
-  	  for(std::list<UdpPacket>::iterator it = udp_packets.begin(); it != udp_packets.end(); it++)
-          { 
+  	    for(std::list<UdpPacket>::iterator it = udp_packets.begin(); it != udp_packets.end(); it++)
+            { 
               count++;
               clock_gettime(CLOCK_MONOTONIC, &ts2);
               double dt = (ts2.tv_sec - ts1.tv_sec)*1000 + (ts2.tv_nsec - ts1.tv_nsec)/1e6;
@@ -166,26 +222,21 @@ int main(int argc, char* argv[])
       #endif
         
             //create cv::Mat from image data
-            //cv::Mat image_m(height, width, CV_8UC1, &it->data[12*4], cv::Mat::AUTO_STEP);
             cv::Mat image_m(cv::Size(width, height), CV_8UC1, const_cast<uint8_t*>(image), width);
-            //cv::Mat image_m = imdecode(image_buf, CV_LOAD_IMAGE_GRAYSCALE);
 
-            //imshow(window_name, image_m);
-            //cv::waitKey(1);
-
-
-            //compute april tag detection
+	    //Detect Tags 
 	    int frame = 0;
 	    double last_t = tic();
             vector<AprilTags::TagDetection> detections = m_tagDetector->extractTags(image_m);
-
+	    
+	    //printf("size of info=%u\n", sizeof(info->dt));
             // print out each detection
-            cout << detections.size() << " tags detected:" << endl;
+            //cout << detections.size() << " tags detected:" << endl;
             for (int i=0; i<detections.size(); i++) {
-                 print_detection(detections[i]);
+                 print_detection(detections[i],&info);
             }
 
-            // show the current image including any detections
+      // show the current image including any detections
       if (m_draw) {
             for (int i=0; i<detections.size(); i++) {
             // also highlight in the image
@@ -195,13 +246,16 @@ int main(int argc, char* argv[])
       }
       if (frame % 10 == 0) {
           double t = tic();
-          cout << "  " << 10./(t-last_t) << " fps" << endl;
+          //cout << "  " << 10./(t-last_t) << " fps" << endl;
           last_t = t;
       }
 
       // exit if any key is pressed
       if (cv::waitKey(1) >= 0) break;
       }
+
+      }
+
     } 
 	
 
