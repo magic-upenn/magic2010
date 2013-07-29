5806e7b0f22a2d1b2225c110d804b8cb9e4df227
James Yang <james.yang92@gmail.com>
2013-07-29 13:16:56 -0400

    NO MORE GIT PROBLEMS

--- drivers/Vision/EasyCamCalib/Interfaces/BinInterfaces/BoldRepError.m
+++
@@ -1,9 +0,0 @@
-function BoldRepError(handles,ind)
-for i=1:20
-    if i==ind
-        set(eval(sprintf('handles.rep%d',i)),'FontWeight','bold');
-    else
-        set(eval(sprintf('handles.rep%d',i)),'FontWeight','normal');
-    end
-end
-drawnow
