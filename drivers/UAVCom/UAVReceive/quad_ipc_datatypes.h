7d52897e1365ae85c6784a25ac06d122cb148ea3
James Yang <james.yang92@gmail.com>
2013-07-29 11:23:24 -0400

    added time fields to publish data

--- drivers/UAVCom/UAVReceive/quad_ipc_datatypes.h
+++ drivers/UAVCom/UAVReceive/quad_ipc_datatypes.h
@@ -2,21 +2,22 @@
 
 //definitions
 #define QUAD_IMU_FORMAT "{double,double,double,double,double,double}"
-#define QUAD_IMG_FORMAT "{int,int,int,<ubyte: 3>}"
+#define QUAD_IMG_FORMAT "{double, int,int,int,<ubyte: 4>}"
 #define APRIL_FORMAT "{ubyte, double, double, double, double, double, double, double, double, [double:9]}"
  
 // struct for IMU over IPC
 typedef struct QuadIMU {
-  float t,roll,pitch,yaw,wroll,wpitch,wyaw,ax,ay,az,p;
+    float t,roll,pitch,yaw,wroll,wpitch,wyaw,ax,ay,az,p;
 } QuadIMU;
 
 // struct for Image over IPC
 typedef struct QuadImg {
-  int width;
-  int height;
-  int dim;
-  uint8_t* image;
-  //  static const uint8_t** returnImageRef() {return &image;}
+    double t;
+    int width;
+    int height;
+    int dim;
+    uint8_t* image;
+    //  static const uint8_t** returnImageRef() {return &image;}
 } QuadImg;
 
 //struct to hold april info that will be published 
