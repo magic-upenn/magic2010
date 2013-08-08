ec2347422b38ca70e996946d3bb218106ef7d924
Patrick Husson <phusson1@umbc.edu>
2013-08-08 11:39:07 -0400

    Added comment in SetMagicPaths about possible error

--- SetMagicPaths.m
+++ SetMagicPaths.m
@@ -1,7 +1,9 @@
+%addpath( [ getenv('MAGIC_DIR') '/ipc' ] )
 addpath( [ getenv('MAGIC_DIR') '/ipc' ] )
 %addpath( [ getenv('MAGIC_DIR') '/Interfaces' ] )
 %addpath( [ getenv('MAGIC_DIR') '/mexSerialization' ] )
 
 
 %addpath( genpath_exclude(getenv('MAGIC_DIR'),'.svn') );
+%If you get errors such as 'Non-existant directory: /ipc' - Uncomment next line
 %addpath( genpath(getenv('MAGIC_DIR')) )
